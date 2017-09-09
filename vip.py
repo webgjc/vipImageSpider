1a6c5ac12095d18a5822c49161404f9258bcf673
gjc <935669873@qq.com>
2017-09-09 10:55:34 +0800

    first version

---
+++ vip.py
@@ -0,0 +1,39 @@
+#coding:utf-8
+#author:ganjiacheng
+import requests
+import json
+import time
+req=requests.Session()
+main=req.get("http://lady.vip.com/")
+brandurl='http://lady.vip.com/index-ajax.php?act=floorBrandList&warehouse=VIP_SH&areaCode=103103&pagecode=a&preview=0&sell_time_from=&time_from=&part=today&page=1&pagesize=1352'
+pplist=req.get(brandurl).text
+ppjson=json.loads(pplist)
+pparr=ppjson['data']['items']
+listfile=open("list.txt",'a')
+j=0
+for item in pparr:
+    print(j)
+    try:
+        url='http://list.vip.com/api-ajax.php?callback=getMerchandiseIds&getPart=getMerchandiseRankList&r='+item['id']
+        sec1=req.get(url)
+        productList=json.loads(sec1.text[18:-1])
+        products=productList['data']['getMerchandiseRankList']['products']
+        for n in range(1,int(len(products)/50)+1):
+            diviProduct=products[50*(n-1):n*50]
+            productIds='%2C'.join(diviProduct)
+            url='http://list.vip.com/api-ajax.php?callback=getMerchandiseDroplets1&getPart=getMerchandiseInfoList&productIds='+productIds+'&r='+item['id']
+            res=req.get(url)
+            jsonstr=json.loads(res.text[24:-1])
+            imgarr=jsonstr['data']['getMerchandiseInfoList']['merchandiseInfoList']
+            for i in imgarr:
+                smallImgurl=i['smallImage']
+                sourceImgurl="http:"+smallImgurl
+                f=open('images/'+str(j)+'.jpg','wb')
+                img=req.get(sourceImgurl).content
+                f.write(img)
+                f.close()
+                print(str(j)+".jpg "+i['productName'],file=open("list.txt",'a'))
+                j+=1
+    except:
+        pass
+    time.sleep(5)
