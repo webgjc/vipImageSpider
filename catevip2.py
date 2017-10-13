0e03409dfbce05303d91e3a686d4aff094e7b5f3
gjc <935669873@qq.com>
2017-10-13 12:33:11 +0800

    改类别版为获取原图，改保存路径分类

--- catevip2.py
+++ catevip2.py
@@ -4,6 +4,7 @@ import json
 import time
 import sys
 import re
+import os
 reload(sys)
 sys.setdefaultencoding('utf-8')
 req=requests.Session()
@@ -13,26 +14,29 @@ pplist=req.get(cateurl).text
 ppjson=json.loads(pplist[12:-1])
 pparr=ppjson['data'][0]['children'][0]['children']
 cate=0
-k=0
 for item in pparr:
+    k=0
     cate+=1
     try:
         searchurl='https://category.vip.com/'+item['url']
         sec1=req.get(searchurl)
+        if not os.path.exists('cateimage/'+str(cate)):
+            os.mkdir("cateimage/"+str(cate))
         #f=open('test.html','w')
         #print >> f,sec1.text.encode('gbk','ignore')
         #m=re.findall(r'data-original="(.*?)" alt="(.*?)"',sec1.text)
         jsonpic=re.findall(r'list = (.*?);',sec1.text)
         piclist=json.loads(jsonpic[0])
-        #print(piclist[0]['name'])
         for j in piclist:
             k+=1
-            f=open('cateimage/'+str(cate)+'-'+str(k)+'.jpg','wb')
-            img=req.get('http:'+j['list_img']).content
+            f=open('cateimage/'+str(cate)+'/'+str(cate)+'-'+str(k).zfill(8)+'.jpg','wb')
+            detail=req.get('https://detail.vip.com/detail-'+str(j['brand_id'])+'-'+str(j['id'])+'.html?f=ad')
+            m=re.search(r'<a href="(.*?)" class="J-mer-bigImgZoom">',detail.text)
+            img=req.get('http:'+m.group(1)).content
             f.write(img)
             f.close()
             file=open("catelist.txt",'a')
-            print >> file,str(cate)+'-'+str(k)+".jpg "+j['name']
+            print >> file,str(cate)+'-'+str(k).zfill(8)+".jpg "+j['name']
         pageall=re.findall(r'<span class="total-item-nums">(.*?)</span>',sec1.text)
         for i in range(2,int(pageall[0][1:-1])+1):
             nexturl=str(item['url']).replace('1.html',str(i)+'.html')
@@ -42,12 +46,14 @@ for item in pparr:
             piclist=json.loads(jsonpic[0])
             for j in piclist:
                 k+=1
-                f=open('cateimage/'+str(cate)+'-'+str(k)+'.jpg','wb')
-                img=req.get('http:'+j['list_img']).content
+                f=open('cateimage/'+str(cate)+'/'+str(cate)+'-'+str(k).zfill(8)+'.jpg','wb')
+                detail=req.get('https://detail.vip.com/detail-'+str(j['brand_id'])+'-'+str(j['id'])+'.html?f=ad')
+                m=re.search(r'<a href="(.*?)" class="J-mer-bigImgZoom">',detail.text)
+                img=req.get('http:'+m.group(1)).content
                 f.write(img)
                 f.close()
                 file=open("catelist.txt",'a')
-                print >> file,str(cate)+'-'+str(k)+".jpg "+j['name']
+                print >> file,str(cate)+'-'+str(k).zfill(8)+".jpg "+j['name']
             print(k)
     except:
         pass
